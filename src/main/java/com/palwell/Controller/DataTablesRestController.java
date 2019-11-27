2508f6b947f5bdfba1c431abd7e0df26acb727fc
Pat Alwell <pat.alwell@gmail.com>
2019-11-27 14:34:14 -0800

    added json support for API results

--- src/main/java/com/palwell/Controller/DataTablesRestController.java
+++ src/main/java/com/palwell/Controller/DataTablesRestController.java
@@ -1,24 +1,37 @@
 package com.palwell.Controller;
+
 import java.util.ArrayList;
 import java.util.List;
-
 import com.palwell.Model.SampleUser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
+
+/*
+To Do:
+
+ */
 
 @RestController
 public class DataTablesRestController {
 
+    private Gson gson = new Gson(); // Or use new GsonBuilder().create();
+
     @RequestMapping("/getUserList")
-    public List<SampleUser> getUserList() {
-        List<SampleUser> userList = new ArrayList<>();
+    public List<String> getUserList() {
+        List<String> userJsonArray = new ArrayList<>();
 
         //Generate 10 Users
         for (int i=0; i < 10; i++) {
+            //create a sample user
             SampleUser user = new SampleUser(String.valueOf(i + 1),
                     String.format("user%d@example.com", i + 1));
-            userList.add(user);
+
+            //convert user to JSON object
+            String jsonUser = gson.toJson(user);
+            userJsonArray.add(jsonUser);
         }
-        return userList;
+
+        return userJsonArray;
     }
 }
--- src/main/java/com/palwell/SpringBootDataTablesApplication.java
+++ src/main/java/com/palwell/SpringBootDataTablesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebAppBdmDataTablesApplication {
+public class SpringBootDataTablesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebAppBdmDataTablesApplication.class, args);
+		SpringApplication.run(SpringBootDataTablesApplication.class, args);
 	}
 
 }
